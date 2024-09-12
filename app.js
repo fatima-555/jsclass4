3a4864aa661a12dee546980da2c4b4edc64a52b1
fatima mustufa <fatimamustufa6@gmail.com>
2024-09-12 12:25:47 -0700

    JS practice1

---
+++ app.js
@@ -0,0 +1,15 @@
+    //p,2
+    var gender = prompt("Enter your gender")
+    var message = ""
+    
+    if(gender === "male"){
+         message = "Welcome boys"
+    }
+    
+    else if(gender === "female"){
+        message = "Welcome sisters"
+    }
+    else {
+        message = "Incorrect value.."
+    }
+    
